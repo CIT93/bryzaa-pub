9046e628c4b3fd8935686bf65bb4fae6f2bcbe19
BryzaAV <0987697@MY.SCCCD.EDU>
2024-01-18 18:46:01 -0800

    coding is done

---
+++ w2/main.js
@@ -0,0 +1,10 @@
+const household = 10
+const housesize = 7
+const groceries = 20
+const dishwasher = 0
+const electronics = 4
+const trash = 40
+const recycle = 20
+const traveling = 12
+const total = household + housesize + groceries + dishwasher + electronics + trash + recycle + traveling
+
