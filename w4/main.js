304d73c1997e7a8478b99d263f2150027113c5d9
BryzaAV <0987697@MY.SCCCD.EDU>
2024-02-05 21:06:04 -0800

    setup for first code along complete

--- w4/main.js
+++ w4/main.js
@@ -38,15 +38,28 @@ function determineHouseHoldPts(numberInHousehold){
 return houseHoldPoints
 }
 
-function start(inputs) {
-const houseHoldPoints = determineHouseHoldPts(inputs[0]);
-const sizeOfHousePoints = determineHouseSizePts(inputs[1]);
-const total = houseHoldPoints + houseHoldPoints;
-cfpData.push([houseHoldMembers, houseSize, houseHoldPoints, HouseSizePts, total]);
+function start(numberInHousehold, size) {
+const houseHoldPoints = determineHouseHoldPts(numberInHousehold);
+const sizeOfHousePoints = determineHouseSizePts(size);
+const total = houseHoldPoints + sizeOfHousePoints;
+cfpData.push([numberInHousehold, size, houseHoldPoints, sizeOfHousePoints, total]);
 }
 
 function displayOutput(){
-    
+    for (arr of cfpData){
+        const output = document.getElementById("output");
+        const newH2 = document.createElement("H2");
+        newH2.textContent = 'Carbon Footprint ${arr[4]}';
+        const newH3 = document.createElement ("h3");
+        newH3.textContent = 'Based on number in and size of home';
+        const newP = document.createElement("p");
+        newP.textContent = 'this number is based on the number of people in the house of ${arr[0]} (score: ${arr[3]}';
+        newP.textContent += 'and a ${arr[1]} size of home (score:${arr[2]}).';
+        output.appendChild(newH2);
+        output.appendChild(newH3);
+        output.appendChild(newP);
+
+    }
 }
 start(5, "apartments");
 start(4, "large");
